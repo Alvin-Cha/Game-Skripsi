a6b1451e826a0d0da2a08ab02d1efd3305361f0d
Alvin.C <alvinschoolemail122@gmail.com>
2026-04-19 21:11:55 +0700

    File Fix

--- scripts/playerMovement.gd
+++ scripts/playerMovement.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const DASH_VELOCITY = 5.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,8 +11,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
